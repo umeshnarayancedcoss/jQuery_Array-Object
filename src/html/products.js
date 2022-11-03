15bff0906df4a7b26eebe512c1bdab7e97c425be
umeshnarayancedcoss <umeshnarayan@cedcoss.com>
2022-11-03 18:57:17 +0530

    jQuery Array & Objects

--- src/html/products.js
+++ src/html/products.js
@@ -1,3 +1,4 @@
+//Initilizing products array----------
 var products = [
     { "id": 101, "name": "Basket Ball", "image": "basketball.png", "price": 150 },
     { "id": 102, "name": "Football", "image": "football.png", "price": 120 },
@@ -5,128 +6,96 @@ var products = [
     { "id": 104, "name": "Table Tennis", "image": "table-tennis.png", "price": 130 },
     { "id": 105, "name": "Tennis", "image": "tennis.png", "price": 100 }
 ];
-
-let cart_array = [];
-
-$(document).ready(function () {
-    var i = 0;
-    var str = "<div>"
-    products.forEach(element => {
-        str += "<div id='product-" + element.id + "' class='product'><img src='images/" + element.image + "'><h3 class='title'><a href='#'>Product " + element.id + "</a></h3><span>Price: $" + element.price + ".00</span><a class='add-to-cart' onclick='cart(" + i++ + ")'>Add To Cart</a></div>"
-        str += "</div>"
-
-        //console.log(str);
-        document.getElementById('products').innerHTML = str;
-    });
-
-
-
+let cart_array = []; //Declaring cart array for cart----------
+// -------------Display Products Dynamically for Add to Cart-------
+var i = 0; //for generating index of the product--
+var str = "<div>"
+products.forEach(element => {
+    str += "<div id='product-" + element.id + "' class='product'><img src='images/" + element.image + "'><h3 class='title'><a href='#'>Product " + element.id + "</a></h3><span>Price: $" + element.price + ".00</span><a class='add-to-cart' value='" + i++ + "'>Add To Cart</a></div>"
+    str += "</div>"
+    document.getElementById('products').innerHTML = str;
 });
-
-// --------------------------display function-----------------
-
-
-function display(){
-
-    var i = 0;
-    var str = "<table><tr><th>Product ID</th><th> Product Name</th><th>Product Image</th><th>Product Price</th><th>Product Quantity<th>Action</th></tr>"
-    cart_array.forEach(element => {
-        var qty = element.quantity;
-        var pro_id=element.id;
-        str += "<tr><td>" + element.id + "</td><td>" + element.name + "</td><td><img src='./images/" + element.image + "' /></td><td> $" + element.price * element.quantity + ".00</td><td><button style='height:30px;width:40px;' onclick='decrement()'>-</button><input type='number' value=" + element.quantity + " style='height:30px;width:30px;' readonly min='0' /><button style='height:30px;width:40px;' onclick='increment(" + qty + ","+pro_id+")' >+</button></td><td><button id='delete'>Remove</Button></td></tr>"
-        str += "</table>"
-        // console.log(str);
-        document.getElementById('table').innerHTML = str;
-
-    })
+// --------------display function for showing cart table-----------------
+function display() {
+    var total = 0; //for total price of the cart------------
+    if (cart_array.length == 0) {
+        total = 0;
+        var table = "<h2>Your cart is empty, please add some products..!!</h2>"
+        document.getElementById('table').innerHTML = table;
+        document.getElementById('total').innerHTML = "";
+    } else {
+        var table = "<button class='empty_cart'>Empty Cart</button><table><tr><th>Product ID</th><th> Product Name</th><th>Product Image</th><th>Product Price</th><th>Product Quantity<th>Action</th></tr>";
+        cart_array.forEach(element => {
+            var qty = element.quantity;
+            var pro_id = element.id;
+            table += "<tr><td>" + element.id + "</td><td>" + element.name + "</td><td><img src='./images/" + element.image + "' /></td><td> $" + element.price * element.quantity + ".00</td><td><button class='decrease' value='" + element.id + "'>-</button><span class='number'>" + element.quantity + " </span><button class='increase' value='" + element.id + "' >+</button></td><td><button class='remove' value='" + element.id + "' >Remove</Button></td></tr>"
+            total = total + (element.price * element.quantity);
+            table += "</table>"
+            document.getElementById('table').innerHTML = table;
+            var t_amount = "<h1>Total: $" + total + ".00 <h1>";
+            document.getElementById('total').innerHTML = t_amount;
+        });
+    }
 }
-
-
-// -----------------------------------------------------------------
-
-function cart(i) {
-    //alert(i);
-    console.log(i);
-
-    //console.log(products[i]); 
-    var id = products[i].id;
-    //  console.log(id);
-    var name = products[i].name;
-    //console.log(name);
-    var price = products[i].price;
-    // console.log(price);
-    var image = products[i].image;
-    // console.log(image);
-
-
-
-    var object = { id: id, name: name, price: price, image: image, quantity: 1 };
-    cart_array.push(object);
-    //console.log(cart_array);
-
-    var i = 0;
-    var str = "<table><tr><th>Product ID</th><th> Product Name</th><th>Product Image</th><th>Product Price</th><th>Product Quantity<th>Action</th></tr>"
-    cart_array.forEach(element => {
-        var qty = element.quantity;
-        var pro_id=element.id;
-        str += "<tr><td>" + element.id + "</td><td>" + element.name + "</td><td><img src='./images/" + element.image + "' /></td><td> $" + element.price * element.quantity + ".00</td><td><button style='height:30px;width:40px;' onclick='decrement(" + qty + ","+pro_id+")'>-</button><input type='number' value=" + element.quantity + " style='height:30px;width:30px;' readonly min='0' /><button style='height:30px;width:40px;' onclick='increment(" + qty + ","+pro_id+")' >+</button></td><td><button id='delete'>Remove</Button></td></tr>"
-        str += "</table>"
-        // console.log(str);
-        document.getElementById('table').innerHTML = str;
-
-    })
-
-}
-
-// -----------------------increment function-----------------------------------
-
-function increment(qty,pro_id) {
-    // alert('ok');
-//  console.log(qty);
-//console.log(pro_id);
-var quantity=qty;
-// alert(quantity);
-
-var pro_id=pro_id;
-
-if(quantity>=0){
-    quantity++;
-        // alert(quantity);
-
-        objIndex = cart_array.findIndex((obj => obj.id == pro_id));
-        //  console.log(objIndex);
-        cart_array[objIndex].quantity =quantity;
-         //console.log("After update: ", cart_array[objIndex])
-       
-     display();
-}
-}
-// -------------------------------------Decrement function-----------------------------------
-
-
-
-function decrement(qty,pro_id) {
-    // alert('ok');
-//  console.log(qty);
-//console.log(pro_id);
-var quantity=qty;
-// alert(quantity);
-
-var pro_id=pro_id;
-
-if(quantity>=0){
-    quantity=quantity-1;
-        //alert(quantity);
-
-        objIndex = cart_array.findIndex((obj => obj.id == pro_id));
-         console.log(objIndex);
-        cart_array[objIndex].quantity =quantity;
-         console.log("After update: ", cart_array[objIndex])
-       
-   display();
-   
-}
-}
-
-////////////////////------------------------------------------
+$(document).ready(function () { //jQuery Starts Here-----
+    // -----------------Add to Cart---------------------------------
+    $(".add-to-cart").on("click", function () {
+        var pro_index_id = $(this).attr("value");
+        var pro_quantity = 1;
+        var pro_price = products[pro_index_id].price;
+        var pro_image = products[pro_index_id].image;
+        var pro_given_id = products[pro_index_id].id;
+        var pro_name = products[pro_index_id].name;
+        if (cart_array.length == 0) { //If cart will be blank-----
+            var object = { index_id: pro_index_id, id: pro_given_id, name: pro_name, image: pro_image, price: pro_price, quantity: pro_quantity };
+            cart_array.push(object);
+        } else if (cart_array.length > 0) { //if cart will not be blank but same product already exists--
+            var flag = 0;
+            cart_array.forEach((element) => {
+                if (element.index_id == pro_index_id) {
+                    flag = 1;
+                    element.quantity = element.quantity + 1;
+                }
+            });
+            if (flag == 0) { //if same product not exists in cart---
+                var object = { index_id: pro_index_id, id: pro_given_id, name: pro_name, image: pro_image, price: pro_price, quantity: pro_quantity };
+                cart_array.push(object);
+            }
+        }
+        display();
+    });
+    // -----------------------Increase Counter--------------------------
+    $(document).on("click", ".increase", function () {
+        var pro_id = $(this).attr("value");
+        var pro_index = cart_array.findIndex((obj => obj.id == pro_id));
+        cart_array[pro_index].quantity = cart_array[pro_index].quantity + 1;  //quantity increase by 1-------
+        display();
+    });
+    // ----------Decrease Counter--------------------------------------------
+    $(document).on("click", ".decrease", function () {
+        var pro_id = $(this).attr("value");
+        var pro_index = cart_array.findIndex((obj => obj.id == pro_id));
+        if (cart_array[pro_index].quantity == 1) { //if quantity will be then it will remove from cart--
+            cart_array.splice(pro_index, 1);
+            alert("Product removed from cart successfully..!!");
+        } else {
+            cart_array[pro_index].quantity = cart_array[pro_index].quantity - 1; //quantity decrease by 1------
+        }
+        display();
+    });
+    // ---------------------Empty Cart-------------------------------------------
+    $(document).on("click", ".empty_cart", function () {
+        cart_array = [];
+        alert("Cart Removed successfully..!!");
+        display();
+    });
+    //-----------------Remove items from cart table-----------------------
+    $(document).on("click", ".remove", function () {
+        var pro_id = $(this).attr("value");
+        var pro_index = cart_array.findIndex((obj => obj.id == pro_id));
+        cart_array.splice(pro_index, 1);
+        alert("Product removed from cart successfully..!!");
+        display();
+    });
+}); //jQuery Ends Here------------------------
 
